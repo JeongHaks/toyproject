f119a366da8b0f9b61a9ee6fd7830df252f01f30
jeonghak.jo <challenge0110@naver.com>
2026-04-14 15:10:13 +0900

    feat:모바일 청첩장 컨트롤러 출력문 추가

--- src/main/java/com/example/toyproject/domain/invitation/controller/view/InvitationViewController.java
+++ src/main/java/com/example/toyproject/domain/invitation/controller/view/InvitationViewController.java
@@ -46,8 +46,7 @@ public class InvitationViewController {
     public String showInvitationView(@PathVariable("code") String code
             , Model model
             , HttpServletRequest request) {
-        System.out.println("모바일 초대장 순서 InvitationViewController 1 : " + code);
-
+        System.out.println("branch 분리 후 commit Test!!!!!!!!!");
         // 코드로 초대장 Entity 조회
         Invitation invitation = invitationService.getInvitationEntityByCode(code);
         // model에 담아서 view로 넘겨준다 코드값
